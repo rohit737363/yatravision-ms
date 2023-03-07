ce9f4c37e28778b0d0d0b58871f0b10306259782
rohit737363 <chougalerohit73@gmail.com>
2023-03-07 21:44:38 +0530

    codepush

---
+++ src/main/java/com/example/yatravision/YatravisionApplication.java
@@ -0,0 +1,13 @@
+package com.example.yatravision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatravisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatravisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yatravision/YatravisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatravision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatravisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
